866de228a92c789f7424c71d94af8a8d5f93d9e6
Adrian Schaedle <schaedles@gmail.com>
2012-07-31 21:56:05 -0400

    holy snakes you can rotate a gameboard

--- game.js
+++ game.js
@@ -1,24 +1,49 @@
-var threeByThree = "###\n# #\n###";
+var threeByThree = ["###",
+                    "# #",+
+                    "###"];
+
 
 var gameElement = document.getElementById("game");
 
 var row = "<"
 
+
 var Board = function(boardString){
     var cargo = {
         height: 0,
         width: 0,
+        map: [],
         tiles: {},
         moves: [],
-        gravity: "south"
+        gravity: "south",
+        prependRow: function(){
+            return this.height += 1;
+        },
     }
     var rows = boardString.split("\n");
     cargo.height = rows.length;
     cargo.width = rows[0].length;
     for (var y in rows.length){
-        // gameElement.
+        // cargo.
     }
     return cargo;
 }
 
+var WOO_GLOBAL_COUNTER = 0;
+var board = document.getElementById("theTable");
+var DIRECTIONS = ["SOUTH","EAST","NORTH","WEST"];
+var DIRECTION_OF_GRAVITY = function(){
+    return DIRECTIONS[Math.abs(WOO_GLOBAL_COUNTER) % 4];
+}
 
+var rotate = function(degrees){
+    
+    WOO_GLOBAL_COUNTER += (degrees >= 0 ) ? 1 : -1;
+    board.style.webkitTransform = "rotate(" + WOO_GLOBAL_COUNTER * 90 + "deg)";
+
+};
+
+var updateBalls = function(){
+
+
+}
