cad6f9fc28ae26b98739edbc42c813895f084f69
Adrian Schaedle <adrian@schaedle.me>
2015-08-07 21:40:00 -0400

    Add sample board

--- Turnabout.elm
+++ Turnabout.elm
@@ -3,23 +3,71 @@ module Turnabout where
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Html exposing (Html)
+import Color exposing (..)
 
-color = "rgb(208, 73, 66)"
+reddish = rgb 208 73 66
+reddishString = "rgb(208, 73, 66)"
 blue = "rgb(32, 22, 129)"
 size = 20
 
+type Cardinality = North | East | South | West
+type Orientation = Top | Right | Bottom | Left
+type Tile = Wall | Block | Marble
+type Rotation = Clockwise | CounterClockwise
+
+type Level = List List Tile
+
+type alias Model =
+  { gravity : Cardinality
+  , board : Level
+  , moves : List Rotation
+  }
+
+-- type alias Marble =
+--   { color : Color
+--   }
+
 main : Html
 main =
-  svg [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95" ]
-    [ rect [ fill color, x "20", y "20", width "40", height "40"] []
-    , rect [ fill color, x "80", y "40", width "40", height "40"] []
-    , block "90" "90"
-    , marble 100 100
+  svg [ version "1.1", x "0", y "0", viewBox "0 0 200 300" ]
+    [ wall (1, 3)
+    , wall (2, 3)
+    , wall (0, 3)
+    , wall (0, 2)
+    , wall (0, 1)
+    , wall (0, 0)
+    , wall (1, 0)
+    , wall (2, 0)
+    , wall (3, 0)
+    , wall (4, 0)
+    , wall (4, 1)
+    , wall (4, 2)
+    , wall (4, 3)
+    , wall (2, 2)
+    , wall (3, 3)
+    , marble (3, 2)
     ]
 
-block : String -> String -> Svg
-block gridX gridY = rect [ fill color, x gridX, y gridY, width "20", height "20"] []
+wall : (Int, Int) -> Svg
+wall (gridX, gridY) =
+  rect
+  [ fill reddishString
+  , x (toString (gridX * size))
+  , y (toString (gridY * size))
+  , width (toString size)
+  , height (toString size)
+  ] []
 
-marble : Int -> Int -> Svg
-marble x y = circle [ fill blue, cx (toString (x + 10)), cy (toString (y + 10)), r "10"] []
+marble : (Int, Int) -> Svg
+marble (x, y) =
+  circle
+    [ fill blue
+    , cx (toString ((x * size) + 10))
+    , cy (toString ((y * size) + 10))
+    , r "10"
+    ] []
 
+-- toFill : Color -> String
+-- toFill color =
+--   case color of
+--     RGBA r g b a -> "rgba(" ++ (toString r) ++ ", 1, 1, 1)"
