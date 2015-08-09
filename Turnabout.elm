9e35984639247afc52cc91fb58410e33a03b38de
Adrian Schaedle <adrian@schaedle.me>
2015-08-09 18:57:42 -0400

    Marginal improvement on handling board rotation

--- Turnabout.elm
+++ Turnabout.elm
@@ -10,9 +10,10 @@ reddishString = "rgb(208, 73, 66)"
 blue = "rgb(32, 22, 129)"
 size = 20
 
+type MarbleColor = Red | Green | Blue
 type Cardinality = North | East | South | West
 type Orientation = Top | Right | Bottom | Left
-type Tile = Wall | Block | Marble
+type Tile = Wall | Block | Marble MarbleColor | Empty
 type Rotation = Clockwise | CounterClockwise
 
 type Level = List List Tile
@@ -23,33 +24,66 @@ type alias Model =
   , moves : List Rotation
   }
 
--- type alias Marble =
---   { color : Color
---   }
+exampleLevel =
+  [ [ Wall,  Wall,       Wall,  Wall]
+  , [ Wall, Empty,      Empty,  Wall]
+  , [ Wall, Empty, Marble Red,  Wall]
+  , [ Wall,  Wall,       Wall,  Wall]
+  ]
+
+levelStart =
+  { gravity = South
+  , board = exampleLevel
+  , moves = []
+  }
+
+type Transform = Rotation Int | Translate Int Int
+
+rotationInDegrees : Rotation -> Int
+rotationInDegrees rotation =
+  case rotation of
+    Clockwise -> 90
+    CounterClockwise -> -90
+    otherwise -> 0
+
+reduceMoves : List Rotation -> Int
+reduceMoves moves = List.foldl (+) 0 (List.map rotationInDegrees moves)
+
+
+transformString : Transform -> String
+transformString trans =
+  case trans of
+    Rotation degrees -> "rotate(" ++ (toString degrees) ++ ")"
+
+someMoves = [CounterClockwise, CounterClockwise, Clockwise]
+someRotation = reduceMoves someMoves
 
 main : Html
 main =
-  svg [ version "1.1", x "0", y "0", viewBox "0 0 200 300" ]
-    [ wall (1, 3)
-    , wall (2, 3)
-    , wall (0, 3)
-    , wall (0, 2)
-    , wall (0, 1)
-    , wall (0, 0)
-    , wall (1, 0)
-    , wall (2, 0)
-    , wall (3, 0)
-    , wall (4, 0)
-    , wall (4, 1)
-    , wall (4, 2)
-    , wall (4, 3)
-    , wall (2, 2)
-    , wall (3, 3)
-    , marble (3, 2)
-    ]
+  svg [ version "1.1", x "0", y "0", viewBox "0 0 300 400"]
+      [
+        g [ transform ("translate(100,100) " ++ (transformString (Rotation someRotation))) ]
+          [ svgWall (1, 3)
+          , svgWall (2, 3)
+          , svgWall (0, 3)
+          , svgWall (0, 2)
+          , svgWall (0, 1)
+          , svgWall (0, 0)
+          , svgWall (1, 0)
+          , svgWall (2, 0)
+          , svgWall (3, 0)
+          , svgWall (4, 0)
+          , svgWall (4, 1)
+          , svgWall (4, 2)
+          , svgWall (4, 3)
+          , svgWall (2, 2)
+          , svgWall (3, 3)
+          , svgMarble (3, 2)
+          ]
+      ]
 
-wall : (Int, Int) -> Svg
-wall (gridX, gridY) =
+svgWall : (Int, Int) -> Svg
+svgWall (gridX, gridY) =
   rect
   [ fill reddishString
   , x (toString (gridX * size))
@@ -58,8 +92,8 @@ wall (gridX, gridY) =
   , height (toString size)
   ] []
 
-marble : (Int, Int) -> Svg
-marble (x, y) =
+svgMarble : (Int, Int) -> Svg
+svgMarble (x, y) =
   circle
     [ fill blue
     , cx (toString ((x * size) + 10))
