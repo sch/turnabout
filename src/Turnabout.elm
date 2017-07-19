a3261372605e778aec993b8c70addac220e902dd
Adrian Schaedle <adrian@schaedle.me>
2017-07-18 23:20:08 -0400

    Fix: prevent SVG from sliding around

--- src/Turnabout.elm
+++ src/Turnabout.elm
@@ -146,7 +146,8 @@ levelSelectView model =
     let
         styles =
             [ ( "font-family", """
-            -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
+            -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial,
+            sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
             """ )
             , ( "color", "#555" )
             , ( "text-align", "center" )
