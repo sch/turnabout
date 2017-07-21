efbe271d2c29f6c97435b772a9b3d281f7605358
Adrian Schaedle <adrian@schaedle.me>
2017-07-20 20:35:55 -0400

    Little note about testing

--- tests/LevelTest.elm
+++ tests/LevelTest.elm
@@ -19,6 +19,26 @@ twoDimensionalList =
     [ [ "a", "b" ], [ "c", "d" ] ]
 
 
+-- SIDENOTE
+--
+-- I kinda hate elm's testing DSL. I was thinking: what do I like? I like
+-- clojure.spec. I like Elm's HTML api. What do I like about these two? Well,
+-- they describe _data_. But something that mimics rspec is not the way. Maybe
+-- this could work?
+--
+--
+-- suite [ test [ description "it can convert a 2D array to a dict of coordinates"
+--              , function Level.toCoordinateDict
+--              , input fixture
+--              , output twoDimensionalList
+--              ]
+
+--       , test [ description "it can ...
+--              , function Level.size
+--              , ...
+--              ]
+--       ]
+
 run =
     always
 
