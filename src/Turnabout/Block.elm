6d95027f6521f3eeb39855ea0a5e3f18a5640a55
Adrian Schaedle <adrian@schaedle.me>
2017-07-27 15:21:30 -0400

    Only render the block shape once

--- src/Turnabout/Block.elm
+++ src/Turnabout/Block.elm
@@ -10,6 +10,7 @@ module Turnabout.Block
 
 import Svg exposing (Svg)
 import Svg.Attributes as Attributes
+import Svg.Lazy exposing (lazy2)
 import Color
 import Color.Convert exposing (colorToHex)
 
@@ -42,13 +43,26 @@ height (Block parts) =
     parts |> List.map Tuple.second |> List.maximum |> Maybe.withDefault 0 |> (+) 1
 
 
+
+-- VIEW
+
+
 view : Int -> ( Block, ( Int, Int ) ) -> Svg msg
 view size ( block, ( x, y ) ) =
+    Svg.g
+        [ Attributes.transform (translateString (x * size + 1) (y * size + 1)) ]
+        [ lazy2 viewShape size block ]
+
+
+viewShape : Int -> Block -> Svg msg
+viewShape size block =
     Svg.rect
         [ Attributes.fill (colorToHex Color.brown)
-        , Attributes.x (toString (x * size + 1))
-        , Attributes.y (toString (y * size + 1))
         , Attributes.width (toString ((width block * size) - 2))
         , Attributes.height (toString ((height block * size) - 2))
         ]
         []
+
+
+translateString x y =
+    "translate(" ++ (toString x) ++ " " ++ (toString y) ++ ")"
