cdea0e890c794238bbe400ed6243620e73517aa1
Adrian Schaedle <adrian@schaedle.me>
2017-08-02 18:40:56 -0400

    Start sketching out recursive path traveling algorithm

--- src/Turnabout/Block.elm
+++ src/Turnabout/Block.elm
@@ -11,7 +11,8 @@ import Svg.Attributes as Attributes
 import Svg.Lazy exposing (lazy2)
 import Color
 import Color.Convert exposing (colorToHex)
-import Turnabout.Coordinate exposing (Coordinate)
+import Turnabout.Coordinate as Coordinate exposing (Coordinate)
+import Turnabout.Direction exposing (Direction(..))
 
 
 type Block
@@ -48,25 +49,49 @@ viewShape size block =
         []
 
 
+type Cursor
+    = Cursor Direction Coordinate
+
+
+{-| This one is a doozy. This function will return a string of points for an Svg
+polygon element. To do so, we get a bit turtle-graphics-y and walk the
+perimeter, obeying a right-hand rule. It walks one unit at a time, incrementing
+the distance by the size parameter. When it reaches "home" (the top-left block,
+we return the beginning position. This means the algorithm builds the svg points
+in reverse.)
+-}
 perimeterPoints : Int -> Block -> String
-perimeterPoints size block =
-    perimeterPointsHelp size block [ ( 1, 1 ) ]
-        |> List.map (\( x, y ) -> (toString x) ++ "," ++ (toString y))
-        |> String.join " "
+perimeterPoints size (Block parts) =
+    let
+        blockToThe : Direction -> Coordinate -> Bool
+        blockToThe direction currentLocation =
+            List.member
+                (Coordinate.byOne direction currentLocation)
+                (( 0, 0 ) :: parts)
 
+        walkPerimeter : Cursor -> List Coordinate
+        walkPerimeter cursor =
+            case cursor of
+                Cursor West ( 1, 0 ) ->
+                    List.singleton ( 1, 1 )
 
-perimeterPointsHelp : Int -> Block -> List Coordinate -> List Coordinate
-perimeterPointsHelp size (Block parts) points =
-    case parts of
-        [] ->
-            [ ( 1, 1 )
-            , ( size - 1, 1 )
-            , ( size - 1, size - 1 )
-            , ( 1, size - 1 )
-            ]
+                Cursor North ( 0, 1 ) ->
+                    List.singleton ( 1, 1 )
 
-        head :: rest ->
-            perimeterPointsHelp size (Block rest) points
+                Cursor South ( x, y ) ->
+                    if blockToThe East ( x, y ) then
+                        ( x * size + 1, y * size + 1 ) :: walkPerimeter (Cursor East ( x + 1, y ))
+                    else if blockToThe West ( x, y ) then
+                        ( x * size + 1, y * size + 1 ) :: walkPerimeter (Cursor West ( x - 1, y ))
+                    else
+                        walkPerimeter (Cursor South ( x, y + size + 1 ))
+
+                Cursor _ _ ->
+                    Debug.crash "Not taking into account all cases"
+    in
+        walkPerimeter (Cursor South ( 1, 1 ))
+            |> List.map (\( x, y ) -> (toString x) ++ "," ++ (toString y))
+            |> String.join " "
 
 
 translateString : number -> number -> String
